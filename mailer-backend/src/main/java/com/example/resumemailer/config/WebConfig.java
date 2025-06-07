3664fc478cc183f4712cafb8cb11cd8fb1fc114a
ankitb2023 <ankitbhujeja2468@gmail.com>
2025-06-07 20:58:28 +0530

    resolved corrs error

--- mailer-backend/src/main/java/com/example/resumemailer/config/WebConfig.java
+++ mailer-backend/src/main/java/com/example/resumemailer/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("http://localhost:3000") // React frontend
+                .allowedOrigins("http://localhost:3000","https://mailer-frontend-gray.vercel.app/") // React frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
